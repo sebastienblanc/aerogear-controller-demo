869b59f7d03b3bebf1375272ad89cea26c85921e
Bruno Oliveira <bruno@abstractj.org>
2012-05-10 07:52:19 -0300

    AuthInfo won't be used anymore

--- src/main/java/org/jboss/aerogear/controller/demo/model/UserLogin.java
+++ src/main/java/org/jboss/aerogear/controller/demo/model/UserLogin.java
@@ -1,8 +1,6 @@
 package org.jboss.aerogear.controller.demo.model;
 
-import org.jboss.aerogear.security.idm.authentication.AuthInfo;
-
-public class UserLogin implements AuthInfo {
+public class UserLogin {
 
     private String username;
     private String password;
@@ -19,24 +17,18 @@ public class UserLogin implements AuthInfo {
         this.role = role;
     }
 
-    @Override
     public Object getPassword() {
         return password;
     }
 
-    @Override
     public String getUsername() {
         return username;
     }
 
-    //TODO must to be fixed
-    @Override
     public String getRole() {
-        //return "customer";
         return role;
     }
 
-    @Override
     public void setRole(String role) {
         this.role = role;
     }
